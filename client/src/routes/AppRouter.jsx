901879fa235e1a2ec2974c7c6d901cf4e88dcc11
DLoi1711 <loiiduongg@gmail.com>
2025-11-07 10:49:50 +0700

    view profile

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -9,23 +9,31 @@ import HomePage from '../pages/HomePage/HomePage';
 import MainLayout from '../layouts/MainLayout';
 import AdminPage from '../pages/AdminPage/AdminPage';
 import { useGet } from '../hooks/useGet';
+import ProfilePage from '../pages/ProfilePage/ProfilePage';
+import EditProfilePage from '../pages/EditProfilePage/EditProfilePage';
 
 const AppRouter = () => {
     const {
-        user
+        user, loading
     } = useGet('/user/profile')
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<MainLayout><HomePage /></MainLayout>} />
-                <Route path="/admin-dashboard" element={<AdminPage currentUser={user}/>} />
+                <Route path="/admin-dashboard" element={<AdminPage currentUser={user} />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/forgot-password" element={<ForgotPasswordPage />} />
                 <Route path="/verify-otp" element={<VerifyOTPPage />} />
                 <Route path="/verify_link" element={<VerifyLinkPage />} />
                 <Route path="/change-password" element={<ChangePassword />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route path='/profile' element={<MainLayout><ProfilePage /></MainLayout>} />
+                <Route path='/profile/editProfile' element={<MainLayout><EditProfilePage /></MainLayout>} />
             </Routes>
         </Router>
     )
